dd9d7ff9b9c4fd4af231a4f980bf1123a0065057
mingy7601 <35078148+mingy7601@users.noreply.github.com>
2026-05-17 02:00:00 -0400

    Added blacklisted categories.

--- backend/app/file_loader.py
+++ backend/app/file_loader.py
@@ -1,11 +1,37 @@
 import json
 from collections import Counter
+import re
 
 #categories [{id, uid, title, modName, recipeCount,recipes}]
+
+BLACKLISTED_CATEGORIES = ["tconstruct:harvest_stats",
+                          "tconstruct:projectile_stats",
+                          "tconstruct:ranged_stats",
+                          re.compile(r"tce_*", re.IGNORECASE),
+                          "thermaldynamics.covers",
+                          "TechReborn.ThermalGenerator",
+                          "TechReborn.PlasmaGenerator", "TechReborn.GasTurbine","TechReborn.DieselGenerator",
+                          "reim.multiblock","projectex.alchemy_table",
+                          re.compile(r"plethora-core:*", re.IGNORECASE),
+                          "packagedauto:package_contents", "compressed_cobblestone", "nae2:cell_view",
+                          re.compile(r"mysticalagriculture:*", re.IGNORECASE),
+                          "justenoughreactors:turbine", "justenoughreactors:reactor", "jeresources.villager",
+                          "if_manual_category", "ie.bottlingMachine", "hatchery.generator.recipe"
+                          ]
 
 def write_file(json_file):
     with open("dump.json", "w") as file:
         json.dump(json_file, file, indent=4)
+
+def is_blacklisted(category_name: str) -> bool:
+    for entry in BLACKLISTED_CATEGORIES:
+        if isinstance(entry, str):
+            if entry == category_name:
+                return True
+        elif isinstance(entry, re.Pattern):
+            if entry.search(category_name):
+                return True
+    return False
 
 def compress_inputs(inputs):
     counts = Counter(item["id"] for item in inputs)
@@ -22,6 +48,8 @@ def load_file(recipes, file_dir):
         data = json.load(file)
 
     for category in data["categories"]:
+        if is_blacklisted(category["uid"]):
+            continue
         uid = category["uid"]
         for recipe in category["recipes"]:
             recipe_id += 1
